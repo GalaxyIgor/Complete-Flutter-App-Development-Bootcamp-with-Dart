7df1accfe834c1e26f3a7c8a2bf52ae478efda95
GalaxyIgor <igornbelisario@gmail.com>
2026-01-13 10:52:30 -0300

    feat hello world container

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -2,11 +2,27 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            color: Colors.white,
+            child: Text("Hello, World!"),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
