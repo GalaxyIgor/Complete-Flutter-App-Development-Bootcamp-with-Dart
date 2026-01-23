9c99fcdf6abdcc3b31d4fcd8ccb83c1e0d160adb
GalaxyIgor <igornbelisario@gmail.com>
2026-01-23 15:15:41 -0300

    feat: Solution 2

--- magic_8_ball/lib/main.dart
+++ magic_8_ball/lib/main.dart
@@ -2,23 +2,49 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: MagicBallPage(),
+        home: BallPage(),
       ),    
     );
 
 
-class MagicBallPage extends StatelessWidget {
-  const MagicBallPage({super.key});
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
 
   @override
   Widget build(BuildContext context) {
   return Scaffold(
     backgroundColor: Colors.blue,
     appBar: AppBar(
-      title: Text('Ask Me Anything'),
+      title: Text(
+        'Ask Me Anything',
+        style: TextStyle(
+          color: Colors.white
+          ),
+        ),
       backgroundColor: Colors.blue.shade900,
+      elevation: 4,
     ),
-    body: null,
+    body: Ball(),
     );
   }
 }
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 2;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('images/ball$ballNumber.png'),
+    );
+  }
+}
+
