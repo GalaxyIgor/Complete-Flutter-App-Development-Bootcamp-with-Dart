e0b4e6c35a8b923d446fa77b24d5d4bc4d91445c
GalaxyIgor <igornbelisario@gmail.com>
2026-01-23 17:40:49 -0300

    feat: create a button that plays a sound

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+                
   @override
   Widget build(BuildContext context) {
+    final player = AudioPlayer();
+    
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: TextButton(
+              onPressed: (){
+                player.play(AssetSource("note1.wav"));
+              },
+              child: Text("Click Me")
+              
+            ),
+          ),
         ),
       ),
     );
