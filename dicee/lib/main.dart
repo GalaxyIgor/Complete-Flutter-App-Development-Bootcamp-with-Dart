bf39ba2752cb294ed3574ed26122e998030946ba
GalaxyIgor <igornbelisario@gmail.com>
2026-01-23 08:35:49 -0300

    Feat: Dicee initial project

---
+++ dicee/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        // Expanded widget to make the image take equal space
+        // First Dice Image
+        Expanded(
+          child: Image.asset('images/dice1.png')
+        ),
+        // Second Dice Image
+        Expanded(
+          child: Image.asset('images/dice2.png')
+        ),
+      ]
+    );
+  }
+} 
