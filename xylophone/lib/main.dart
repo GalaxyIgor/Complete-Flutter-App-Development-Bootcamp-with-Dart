b36eae2c20f573ae16acd3c12ce3eab77186308b
GalaxyIgor <igornbelisario@gmail.com>
2026-01-23 18:20:22 -0300

    feat: finish my app

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -11,18 +11,96 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final player = AudioPlayer();
     
+    final colors = [
+      const Color(0xFF0FC2C0),
+      const Color(0xFF0CABA8),
+      const Color(0xFF008F8C),
+      const Color(0xFF017A78),
+      const Color(0xFF016563),
+      const Color(0xFF015958),
+      const Color(0xFF014443),
+    ];
+
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: (){
-                player.play(AssetSource("note1.wav"));
-              },
-              child: Text("Click Me")
-              
+        appBar: AppBar(
+          title: const Text(
+            'Green Xylophone App',
+            style: TextStyle(
+              color: Colors.white
             ),
           ),
+          centerTitle: true,
+          backgroundColor: const Color(0xFF014443),
+          elevation: 4,
+          shadowColor: Colors.black,
+        ),
+        backgroundColor: const Color(0xFF014443),
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    player.play(AssetSource('note1.wav'));
+                  },
+                  child: Container(color: colors[0]),
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    player.play(AssetSource('note2.wav'));
+                  },
+                  child: Container(color: colors[1]),
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    player.play(AssetSource('note3.wav'));
+                  },
+                  child: Container(color: colors[2]),
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    player.play(AssetSource('note4.wav'));
+                  },
+                  child: Container(color: colors[3]),
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    player.play(AssetSource('note5.wav'));
+                  },
+                  child: Container(color: colors[4]),
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    player.play(AssetSource('note6.wav'));
+                  },
+                  child: Container(color: colors[5]),
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: () {
+                    player.play(AssetSource('note7.wav'));
+                  },
+                  child: Container(color: colors[6]),
+                ),
+              ),
+            ],
+          )
+
         ),
       ),
     );
