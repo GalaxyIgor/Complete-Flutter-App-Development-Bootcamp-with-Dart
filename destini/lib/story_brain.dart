2dad592dfdf27b4f6e4864b6e896c74757e33c40
GalaxyIgor <igornbelisario@gmail.com>
2026-01-27 18:13:39 -0300

    feat: step 8

--- destini/lib/story_brain.dart
+++ destini/lib/story_brain.dart
@@ -1,6 +1,6 @@
 import 'story.dart';
 class StoryBrain {
-//TODO: Step 7 - Uncomment the lines below to include storyData as a private property in StoryBrain. Hint: You might need to change something in story.dart to make this work.
+  int storyNumber = 0;
 
   final List<Story> _storyData = [
     Story(
@@ -37,8 +37,9 @@ class StoryBrain {
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
-//TODO: Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.
-
+  String getStory(){
+    return _storyData[storyNumber].storyTitle;
+  }
 //TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
 
 //TODO: Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
