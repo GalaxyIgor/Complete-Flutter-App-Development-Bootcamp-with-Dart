f2c70b8cf9f0c51232a7a0c7aa093065518f3341
GalaxyIgor <igornbelisario@gmail.com>
2026-02-04 11:13:20 -0300

    chore: Final vs. Const

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -8,16 +8,67 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  final Color customCardColor = Color(0xFF1D1E33);
+  final Color customButtonColor = Color(0xFFEB1555);
+  final double customButtonHeight = 80.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
+      body: Column(
+        children: [
+          ReusableCard(colour: customCardColor),
+          Expanded(
+            child: Row(
+              children: [
+                ReusableCard(colour: customCardColor),
+                ReusableCard(colour: customCardColor),
+              ],
+            ),
+          ),
+          ReusableCard(colour: customCardColor),
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.only(top: 10.0),
+            color: customButtonColor,
+            width: double.infinity,
+            height: customButtonHeight,
+            child: Text(
+              'CALCULATE',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          )
+        ],
+      )
       
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color colour;
+  
+  const ReusableCard({
+    required this.colour,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
     );
   }
 }
